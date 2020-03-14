512ab23eca7017007f53d265fad1733be46ed0da
gasparchan <62165020+gasparchan@users.noreply.github.com>
2020-03-15 02:42:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+git clone https://github.com/rosywrt/luci-theme-rosy
